c96c3de8c1905b62320f3127a287fb49f96255df
Phani Kumar Tumuluri <ptumuluri@ptumuluri.hyd.corp.google.com>
2017-05-10 16:16:30 +0530

    added service worker listenrers

--- public/js/app.js
+++ public/js/app.js
@@ -14,4 +14,28 @@
     console.log('Update to a modern browser!');
   }
 
+  //After DOM Loaded
+  document.addEventListener('DOMContentLoaded', function(event) {
+    toast("online");
+    //On initial load to check connectivity
+    if (!navigator.onLine) {
+      updateNetworkStatus();
+    }
+
+    window.addEventListener('online', updateNetworkStatus, false);
+    window.addEventListener('offline', updateNetworkStatus, false);
+  });
+
+  //To update network status
+  function updateNetworkStatus() {
+    let header = document.querySelector('header');
+    if (navigator.onLine) {
+      header.classList.remove('app__offline');
+    }
+    else {
+      header.classList.add('app__offline');
+      toast('You are now offline..');
+    }
+  }
+
 })();
