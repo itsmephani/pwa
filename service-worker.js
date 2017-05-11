c192c16dded6b9bcc3e9376c304e32628ac1def7
Phani Kumar Tumuluri <ptumuluri@ptumuluri.hyd.corp.google.com>
2017-05-11 17:33:22 +0530

    added content editor functionality to the app

--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,14 @@
 
    self.addEventListener('install', function(event) {
     // Perform some task
-    console.log('Service worker');
-    console.log(event);
+    //console.log('Service worker');
+    //console.log(event);
    });
 
    self.addEventListener('activate', function(event) {
     // Perform some task
-    console.log('Service worker activated');
-    console.log(event);
+    //console.log('Service worker activated');
+    //console.log(event);
    });
 
 
@@ -18,8 +18,7 @@
     var cacheName = 'v1.0';
 
     var filesToCache = [
-        './',
-        './public/css/style.css',
+        // './public/css/style.css',
         './public/css/toast.css',
         './public/js/app.js',
         './public/js/toast.js'
@@ -28,13 +27,13 @@
     // Install Service Worker
     self.addEventListener('install', function(event) {
 
-        console.log('Service Worker: Installing....');
+        //console.log('Service Worker: Installing....');
 
         event.waitUntil(
 
             // Open the Cache
             caches.open(cacheName).then(function(cache) {
-                console.log('Service Worker: Caching App Shell at the moment......');
+                //console.log('Service Worker: Caching App Shell at the moment......');
 
                 // Add Files to the Cache
                 return cache.addAll(filesToCache);
@@ -46,13 +45,13 @@
     // Fired when the Service Worker starts up
     self.addEventListener('activate', function(event) {
 
-        console.log('Service Worker: Activating....');
+        //console.log('Service Worker: Activating....');
 
         event.waitUntil(
             caches.keys().then(function(cacheNames) {
                 return Promise.all(cacheNames.map(function(key) {
                     if( key !== cacheName) {
-                        console.log('Service Worker: Removing Old Cache', key);
+                        //console.log('Service Worker: Removing Old Cache', key);
                         return caches.delete(key);
                     }
                 }));
@@ -64,9 +63,9 @@
 
     self.addEventListener('fetch', function(event) {
 
-        console.log('Service Worker: Fetch', event.request.url);
+        //console.log('Service Worker: Fetch', event.request.url);
 
-        console.log("Url", event.request.url);
+        //console.log("Url", event.request.url);
 
         event.respondWith(
             caches.match(event.request).then(function(response) {
